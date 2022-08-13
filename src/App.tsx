1cfbf843ff664dec013884e2676ca0f74d6d55be
jinwoo <wwo1278@gmail.com>
2022-08-13 10:56:50 +0900

    feat: 리액트 쿼리 example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { SignUpPage, LoginPage, MainPage } from 'page';
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
+
+import { SignUpPage, LoginPage, MainPage } from 'page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <Wrapper>
-        <Routes>
-          <Route path='/*' element={<MainPage />} />
-          <Route path='/auth/signup' element={<SignUpPage />} />
-          <Route path='/auth/login' element={<LoginPage />} />
-          <Route path='*' element={<div>404 NOT FOUND</div>} />
-        </Routes>
-      </Wrapper>
+      <QueryClientProvider client={queryClient}>
+        <Wrapper>
+          <Routes>
+            <Route path='/*' element={<MainPage />} />
+            <Route path='/auth/signup' element={<SignUpPage />} />
+            <Route path='/auth/login' element={<LoginPage />} />
+            <Route path='*' element={<div>404 NOT FOUND</div>} />
+          </Routes>
+        </Wrapper>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
